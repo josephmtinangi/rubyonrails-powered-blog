1d0303710387443719548ca118502ec8fd59cf66
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-08 18:26:45 +0300

    Update a category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -22,6 +22,17 @@ class Admin::CategoriesController < Admin::ApplicationController
   end
 
   def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update_attributes(category_params)
+      flash[:notice] = 'Category updated'
+      redirect_to admin_categories_path
+    else
+      render 'edit'
+    end
   end
 
   def delete
