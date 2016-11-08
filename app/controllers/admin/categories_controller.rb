be6ce0dcad3175ae07e0cbacf098e04126fd77c2
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-08 18:29:28 +0300

    Delete a category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -14,7 +14,7 @@ class Admin::CategoriesController < Admin::ApplicationController
   def create
     @category = Category.new(category_params)
     if @category.save
-      flash[:notice] = 'Category added successfully'
+      flash[:notice] = 'Category added successfully.'
       redirect_to admin_categories_path
     else
       render 'new'
@@ -28,14 +28,18 @@ class Admin::CategoriesController < Admin::ApplicationController
   def update
     @category = Category.find(params[:id])
     if @category.update_attributes(category_params)
-      flash[:notice] = 'Category updated'
+      flash[:notice] = 'Category updated.'
       redirect_to admin_categories_path
     else
       render 'edit'
     end
   end
 
-  def delete
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    flash[:notice] = 'Category removed.'
+    redirect_to admin_categories_path
   end
 
   private
