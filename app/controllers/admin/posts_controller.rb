40eeef9b12f91b8cb4badc973b2de780bc5b6c01
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-08 19:35:15 +0300

    Posts and Users CRUD

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,16 +1,48 @@
 class Admin::PostsController < Admin::ApplicationController
   def index
+    @posts = Post.all
   end
 
   def show
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = 'Post added successfully.'
+      redirect_to admin_posts_path
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
-  def delete
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      flash[:notice] = 'Post updated'
+      redirect_to admin_posts_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:notice] = 'Post deleted'
+    redirect_to admin_posts_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :category_id, :user_id, :image, :tags, :body)
   end
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,16 +1,33 @@
 class Admin::UsersController < Admin::ApplicationController
   def index
+    @users = User.all
   end
 
   def show
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = 'User added.'
+      redirect_to admin_users_path
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
   def delete
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password_hash, :password_salt)
   end
 end
